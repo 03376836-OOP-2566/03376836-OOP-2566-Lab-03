cae4855926a1d1f1e4516fb41a425cba0d680702
SitthaKlaphanich <ao2604152014@hotmail.com>
2024-02-17 01:38:16 +0700

    Lab03_Exercise02_result.md

--- 0258/Lab03_Exercise01/obj/Debug/net8.0/Lab03_Exercise01.AssemblyInfo.cs
+++ 0258/Lab03_Exercise01/obj/Debug/net8.0/Lab03_Exercise01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab03_Exercise01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9f16efb816616a77b7d632e681ae556c43d4176")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb9d70306299c08d95b461101902ff27ff1499cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab03_Exercise01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab03_Exercise01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
