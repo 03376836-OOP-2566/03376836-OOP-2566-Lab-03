91d4ab21a75c07271ab6c6b1d0862eebc1db95a9
Sorawit255 <65030255@kmitl.ac.th>
2024-02-20 18:59:27 +0700

    1

---
+++ 0255/Lab03_Exercise01/Program.cs
@@ -0,0 +1,4 @@
+﻿System.Console.WriteLine("Hello, I'm 65030255");
+System.Console.WriteLine("Please enter your name");
+var name = System.Console.ReadLine();  
+System.Console.WriteLine($"Your name is {name}");
