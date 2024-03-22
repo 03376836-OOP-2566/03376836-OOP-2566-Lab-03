7f401bd0e0a0b48934ead2011d57cd15ff4624c9
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-23 02:19:28 +0700

    commit 9.1/9.2

---
+++ Lab_result/Lab03_Exercise09/Program.cs
@@ -0,0 +1,10 @@
+﻿var sb = new System.Text.StringBuilder();
+
+// Create a string composed of numbers 0 - 9
+for (int i = 0; i < 10; i++)
+{
+    System.Console.Write ($"Loop {i+1, 2} : ");
+    sb.Append(i.ToString());
+    System.Console.WriteLine($"sb = {sb.ToString(),-11}, sb.GetHashCode() = {sb.GetHashCode()}");
+}
+Console.WriteLine($"Finally : sb = {sb}");  
