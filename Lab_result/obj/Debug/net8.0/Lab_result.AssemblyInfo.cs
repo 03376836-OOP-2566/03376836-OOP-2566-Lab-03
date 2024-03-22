5cabcd6183cda825089bba42497439f99caee03e
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-23 00:16:32 +0700

    change

--- Lab_result/obj/Debug/net8.0/Lab_result.AssemblyInfo.cs
+++ Lab_result/obj/Debug/net8.0/Lab_result.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab_result")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf16c04a54259b32166815007d1bd36d8bcd859e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab_result")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab_result")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
