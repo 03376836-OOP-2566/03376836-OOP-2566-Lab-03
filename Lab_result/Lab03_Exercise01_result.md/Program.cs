967b45674c77bf86fc82be440d144e307eab70da
65030179179Pattarapon <144198506+65030179179Pattarapon@users.noreply.github.com>
2024-03-22 23:49:43 +0700

    Update Program.cs

--- Lab_result/Lab03_Exercise01_result.md/Program.cs
+++ Lab_result/Lab03_Exercise01_result.md/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("C:\Users\Admin\OneDrive\รูปภาพ\Saved Pictures\03376836-OOP-LAB\Week-03");
+"C:\Users\Admin\OneDrive\รูปภาพ\Saved Pictures\03376836-OOP-LAB\Week-03"
 
