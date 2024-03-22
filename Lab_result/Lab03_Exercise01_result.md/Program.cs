b051cfd29096a1a99fa3ac5df15c05dc117649ed
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-22 23:48:27 +0700

    Ex1

--- Lab_result/Lab03_Exercise01_result.md/Program.cs
+++ Lab_result/Lab03_Exercise01_result.md/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("C:\Users\Admin\OneDrive\รูปภาพ\Saved Pictures\03376836-OOP-LAB\Week-03");
+
--- Lab_result/obj/Debug/net8.0/Lab_result.AssemblyInfo.cs
+++ Lab_result/obj/Debug/net8.0/Lab_result.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab_result")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d88ece497316933ae8bc9735be7a797e8dd9fb3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab_result")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab_result")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
