773206521af46fd69f35a0f72d597070584b76a4
65030179179Pattarapon <144198506+65030179179Pattarapon@users.noreply.github.com>
2024-03-22 23:57:38 +0700

    Update Program.cs

--- Lab_result/Lab03_Exercise01_result.md/Program.cs
+++ Lab_result/Lab03_Exercise01_result.md/Program.cs
@@ -1,7 +1,1 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        object value = ! [pic1](c: \Users\Admin\OneDrive\รูปภาพ\Saved Pictures\03376836 - OOP - LAB\Week - 03\Ex1.png);
-    }
-}
+![pic1](c: \Users\Admin\OneDrive\รูปภาพ\Saved Pictures\03376836 - OOP - LAB\Week - 03\Ex1.png)
