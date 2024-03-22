2da7bf77995c8fd71f51dba2f6c35485a0e30b7d
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-23 00:32:58 +0700

    แก้ไขไฟล์ program Ex2

--- Lab_result/Lab03_Exercise02/Program.cs
+++ Lab_result/Lab03_Exercise02/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+string s1 = new string("Hello, World!");
+string s2 = "Hello, World!";
+var s3 = "Hello, World!";
+string s4 = new("Hello, World!");
+Console.WriteLine(s1);
+Console.WriteLine(s2);
+Console.WriteLine(s3);
+Console.WriteLine(s4);
