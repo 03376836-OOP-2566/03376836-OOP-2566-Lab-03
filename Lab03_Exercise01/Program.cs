d9727c42e0d10dff9ef597c68a26aa0ca3678f7f
newyork <spidermanwin1@gmail.com>
2024-03-24 01:42:24 +0700

    Finishex1

--- Lab03_Exercise01/Program.cs
+++ Lab03_Exercise01/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Hello, I'm 65030226");
+System.Console.WriteLine("Please enter your name");
+var name = System.Console.ReadLine();  
+System.Console.WriteLine($"Your name is {name}");
