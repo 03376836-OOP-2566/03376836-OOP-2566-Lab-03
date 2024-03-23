ee5ed51dcec4a0a2748c1815d5c0f3af19358e02
nitiphum <65030130@kmitl.ac.th>
2024-03-23 21:14:27 +0700

    สร้างex3

--- Lab03_Exercise02/Program.cs
+++ Lab03_Exercise02/Program.cs
@@ -1,9 +1,15 @@
-﻿using System;
-string s1 = new string("Hello, World!");
-string s2 = "Hello, World!";
-var s3 = "Hello, World!";
-string s4 = new("Hello, World!");
-Console.WriteLine(s1);
-Console.WriteLine(s2);
-Console.WriteLine(s3);
-Console.WriteLine(s4);
+﻿string s1 = "A string is more ";
+string s2 = "than the sum of its chars.";
+
+System.Console.WriteLine("Before Concatenate of s1 and s2");
+System.Console.WriteLine($"s1 = \"{s1}\", Hash code = {s1.GetHashCode():X}");
+System.Console.WriteLine($"s2 = \"{s2}\", Hash code = {s2.GetHashCode():X}");
+// Concatenate s1 and s2. This actually creates a new
+// string object and stores it in s1, releasing the
+// reference to the original object.
+
+s1 += s2;
+
+System.Console.WriteLine("After Concatenate of s1 and s2");
+System.Console.WriteLine($"s1 = \"{s1}\", Hash code = {s1.GetHashCode():X}");
+System.Console.WriteLine($"s2 = \"{s2}\", Hash code = {s2.GetHashCode():X}");
