03faef4b00be3c80ebe63325c60b4f57cd2200c4
nitiphum <65030130@kmitl.ac.th>
2024-03-23 21:45:15 +0700

    สร้างex9

--- Lab03_Exercise09/Program.cs
+++ Lab03_Exercise09/Program.cs
@@ -1,10 +1,10 @@
-﻿System.Text.StringBuilder sb = new System.Text.StringBuilder("Rat is my favorite animal.");
+﻿var sb = new System.Text.StringBuilder();
 
-System.Console.WriteLine(@$"Before change : sb = {sb.ToString()}, 
-sb.GetHashCode = {sb.GetHashCode()}");
-
-System.Console.WriteLine(@"Replace first char in sb  from R to C");
-sb[0] = 'C';
-
-System.Console.WriteLine(@$"After change : sb = {sb.ToString()}, 
-sb.GetHashCode = {sb.GetHashCode()}");
+// Create a string composed of numbers 0 - 9
+for (int i = 0; i < 10; i++)
+{
+    System.Console.Write ($"Loop {i+1, 2} : ");
+    sb.Append(i.ToString());
+    System.Console.WriteLine($"sb = {sb.ToString(),-11}, sb.GetHashCode() = {sb.GetHashCode()}");
+}
+Console.WriteLine($"Finally : sb = {sb}");  
