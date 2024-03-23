37d8ef00913a37f9b9d0e1cdd70767d298c20c20
nitiphum <65030130@kmitl.ac.th>
2024-03-23 21:42:28 +0700

    สร้างex9

---
+++ Lab03_Exercise09/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Text.StringBuilder sb = new System.Text.StringBuilder("Rat is my favorite animal.");
+
+System.Console.WriteLine(@$"Before change : sb = {sb.ToString()}, 
+sb.GetHashCode = {sb.GetHashCode()}");
+
+System.Console.WriteLine(@"Replace first char in sb  from R to C");
+sb[0] = 'C';
+
+System.Console.WriteLine(@$"After change : sb = {sb.ToString()}, 
+sb.GetHashCode = {sb.GetHashCode()}");
