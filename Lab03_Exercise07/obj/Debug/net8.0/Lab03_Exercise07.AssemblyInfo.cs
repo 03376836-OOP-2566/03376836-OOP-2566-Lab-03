ea853adbf370fabb5c739fe31d84f99b76ddcad5
nitiphum <65030130@kmitl.ac.th>
2024-03-23 21:36:27 +0700

    coomitex7

--- Lab03_Exercise07/obj/Debug/net8.0/Lab03_Exercise07.AssemblyInfo.cs
+++ Lab03_Exercise07/obj/Debug/net8.0/Lab03_Exercise07.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab03_Exercise07")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d408e0b9016ee071dafb76f5efbcb4003373b728")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d42d05004c1c40727d4a702498bc82c829295080")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab03_Exercise07")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab03_Exercise07")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
